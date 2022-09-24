660a7769280b3ba3b56eefb46375f23698fd6f3f
Harry6234 <harry.ambitions@gmail.com>
2022-09-24 19:10:46 +0000

    update commit

--- refresher_training_survey.R
+++ refresher_training_survey.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(tidyverse)
 library(readxl)
 library(janitor)
+library("gridExtra")
 
 
 getwd()
@@ -43,28 +44,109 @@ survey$`Questionaire Number` <- NULL
 skimr::skim(survey)
 
 # ------Filtering respondents who have had similar training
-had_similar_training <- survey$`Have you attended any training of this sort before?`== "Yes"
-survey[had_similar_training,]
+similar_training <- survey$`Have you attended any training of this sort before?`== "Yes"
+survey[similar_training,]
+
+
 
 # ------Filtering respondents who have not had similar training
 no_similar_training <- survey$`Have you attended any training of this sort before?`=="No"
 survey[no_similar_training,]
 
 
-#Cross tab analysis with percentages.
-tabyl(survey, College, Designation) %>%
-  adorn_percentages("row") %>%
-  adorn_pct_formatting(digits = 1)
 
 
-# Filtering respondents
-table(survey$`Have you attended any training of this sort before?`)
+# -------------Filtering respondents-----------
 
 #attended a similar training
 attended_similar <- (subset(survey, survey$`Have you attended any training of this sort before?`== "Yes"))
+similar_training <- ggplot(attended_similar, aes(College, `The training session was useful to my work`, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.5) +
+  labs(title ="The training session was useful to my work",
+       subtitle="Respondents who have attended a similar training") +
+  ylab("")
+
 
 #Never attended a similar training
 not_attended_similar <- (subset(survey, survey$`Have you attended any training of this sort before?`== "No"))
+no_similar_training <- ggplot(not_attended_similar, aes(College, `The training session was useful to my work`, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(subtitle="Respondents who have not attended a similar training",
+       caption = "Source: August 2022 Refresher Training Survey, University of Cape Coast") +
+  ylab("")
+
+#------------
+#subplot of attendance of similar training and no similar training
+grid.arrange(similar_training, no_similar_training, nrow = 2, ncol=1)
+#--------------------------------------------------------------------
+
+similar_training_impact <- ggplot(attended_similar, aes(College, `The training will impact positively on my work `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.5) +
+  labs(title="The training will impact positively on my work",
+       subtitle="Respondents attended a similar training and believes this training will impact positively on their work") +
+  ylab("")
+
+no_similar_training_impact <- ggplot(not_attended_similar, aes(College, `The training will impact positively on my work `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.9) +
+  labs(subtitle="Respondents have not attended a similar training and believes this training will impact positively on their work",
+       caption = "Source: August 2022 Refresher Training Survey, University of Cape Coast") +
+  ylab("")
+
+#-------
+#subplot of attendance of similar training and no similar training Vs Impact
+grid.arrange(similar_training_impact, no_similar_training_impact, nrow = 2, ncol=1)
+# -----------------
+
+similar_and_regular_training <- ggplot(attended_similar, aes(College,`I need such training regularly `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.5) +
+  labs(title="I need such training regularly",
+       subtitle="Attended Similar training: Needs such training regularly") +
+  ylab("")
+
+no_similar_and_regular_training <- ggplot(not_attended_similar, aes(College,`I need such training regularly `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(subtitle="Has not attendened similar training: Needs such training regularly",
+       caption = "Source: August 2022 Refresher Training Survey, University of Cape Coast") +
+  ylab("")
+#-------------
+grid.arrange(similar_and_regular_training, no_similar_and_regular_training, nrow = 2, ncol=1)
+#-----------------
+
+
+# acquired basic skills for UCCOSIS Usage
+
+acquired_basic_knowledge1 <- ggplot(attended_similar, aes(College,`I have acquired the basic knowledge necessary to use the UCCOSIS  `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.4) +
+  labs(title="I have acquired the basic knowledge necessary to use the UCCOSIS",
+       subtitle="Perpective: Attended Similar training") +
+  ylab("")
+
+acquired_basic_knowledge2 <- ggplot(not_attended_similar, aes(College,`I have acquired the basic knowledge necessary to use the UCCOSIS  `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(subtitle="Perpective: Has not attended Similar training",
+       caption = "Source: August 2022 Refresher Training Survey, University of Cape Coast")+
+  ylab("")
+
+#-------------
+grid.arrange(acquired_basic_knowledge1, acquired_basic_knowledge2, nrow = 2, ncol=1)
+# ---------
+
+
+# Needs a user manual for UCCOSIS
+
+needs_user_manual_1 <- ggplot(attended_similar, aes(College,`I need a user manual for the UCCOSIS `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.4) +
+  labs(title="I need a user manual for the UCCOSIS",
+       subtitle="Perpective: Attended Similar training")+
+  ylab("")
+
+needs_User_manual_2 <- ggplot(not_attended_similar, aes(College,`I need a user manual for the UCCOSIS `, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(subtitle="Perpective: Has not attended Similar training",
+       caption = "Source: August 2022 Refresher Training Survey, University of Cape Coast")+
+  ylab("")
+
+grid.arrange(needs_user_manual_1, needs_User_manual_2, nrow = 2, ncol=1)
 
 # ------- Insight 1: This insight compares Respondents who have had attended 
 # similar training and how relevant the Refresher training was to their work" -----------
@@ -111,13 +193,42 @@ ggplot(survey, aes(College, survey$`I have acquired the basic knowledge necessar
 
 
 
-# Venue suitability Vs Similar event attended
-Attended_similar_training <- survey$`Have you attended any training of this sort before?`
+# Insight 5--- Venue suitability Vs Similar event attended
 Venue_Suitability <- survey$`Venue Suitability`
-c <- ggplot(survey, aes(Venue_Suitability))
-c + geom_bar(aes(fill=Attended_similar_training, fill=College), width = 0.5)+
-    theme(axis.text.x = element_text(angle=0, vjust=0.5)) +
-  labs(title="comparisons", 
-       subtitle="Note: NA implies No Answer", 
+insight_5 <- ggplot(survey, aes(College, Venue_Suitability, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.9) +
+  labs(title="Venue Suitablity for the Refresher Training", 
+       subtitle="Slide Note: Respondents' response on how suitable the venue was for the training", 
        caption="Source: Survey Response From Refresher Training,University of Cape Coast; August 2022")
 View(survey)
+
+# Insight 6 ----- How respondents rated each presentation made during the training----
+present_1 <- ggplot(survey, aes(College,`How would you rate the presentation 1?`, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(title="How did respondents find the Presentations made throughout the training sessions", 
+       subtitle="Slide Note: Respondents' judgement on the quality of presentations made for the training")+
+  ylab("Presentation 1")+ xlab("College") 
+# --
+present_2 <- ggplot(survey, aes(College,`How would you rate the presentation 2?`, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(caption="Source: August 2022 Refresher Training Survey, University of Cape Coast")+
+  ylab("Presentation 2")+ xlab("College") 
+# ---
+present_3 <- ggplot(survey, aes(College,`How would you rate the presentation 3?`, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(caption="Source: August 2022 Refresher Training Survey, University of Cape Coast")+
+  ylab("Presentation 3")+ xlab("College") 
+#---
+present_4 <- ggplot(survey, aes(College,`How would you rate the presentation 4?`, fill=Designation)) +
+  geom_bar(stat="identity", position = "dodge", width = 0.8) +
+  labs(caption="Source: August 2022 Refresher Training Survey, University of Cape Coast")+
+  ylab("Presentation 4")+ xlab("College") 
+
+#working on subplots of two plots per view 
+
+grid.arrange(present_1, present_2, nrow = 2, ncol=1)  
+grid.arrange(present_3, present_4, nrow= 2, ncol=1)
+
+
+
+#-------------------------------------------------
